21aaf3239203b57a821d65985077248278c315f0
hillnat <hillgernate@gmail.com>
2024-12-17 10:20:10 -0800

    a

---
+++ 2024WinterJamSpriteGame/Assets/Scripts/ViewManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+    public Transform dialogueRoot;
+    public Transform mainGameRoot;
+
+    #region Singleton
+    public static ViewManager instance;
+    private void Singleton()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+
+        instance = this;
+    }
+    #endregion
+    private void Awake()
+    {
+        Singleton();
+    }
+    public void SwitchToDialogue()
+    {
+        dialogueRoot.gameObject.SetActive(true);
+        mainGameRoot.gameObject.SetActive(false);
+    }
+    public void SwitchToMainGame()
+    {
+        dialogueRoot.gameObject.SetActive(false);
+        mainGameRoot.gameObject.SetActive(true);
+    }
+}
