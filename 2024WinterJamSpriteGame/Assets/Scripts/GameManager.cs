0f9e79af35ffab5fbbc1726a7e80eb54d10c2081
studentPun <s239053@students.aie.edu>
2024-12-17 09:53:26 -0800

    Pause Menu Functional

---
+++ 2024WinterJamSpriteGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    /*
+    private static GameManager instance;
+    public static GameManager GetInstance(){
+        if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+    }
+    */
+
+    //Global variables
+    public static float masterVolume = 1.0f;
+    public static float musicVolume = 0.33f;
+    public static float sfxVolume = 0.5f;
+}
