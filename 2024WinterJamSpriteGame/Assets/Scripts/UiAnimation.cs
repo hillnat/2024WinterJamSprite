d07645728ff9e64e98c3c5a4090bcfd880bc6867
hillnat <hillgernate@gmail.com>
2024-12-17 16:22:40 -0800

    a

---
+++ 2024WinterJamSpriteGame/Assets/Scripts/UiAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAnimation : MonoBehaviour
+{
+    public List<Sprite> frames;
+    public float frameDelay=0.1f;
+    private Image image;
+
+    public void RunAnim()
+    {
+        StartCoroutine(anim());
+    }
+
+    IEnumerator anim()
+    {
+        image.enabled = true;
+        for (int i=0; i<frames.Count; i++)
+        {
+            image.sprite = frames[i];
+            yield return new WaitForSeconds(frameDelay);
+        }
+        image.sprite = frames[0];
+        image.enabled = false;
+    }
+}
